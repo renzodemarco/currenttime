4eb36541a9af666bcf3a96b50ca0b6b0b736748f
Renzo Demarco <renzo11demarco@gmail.com>
2023-01-25 18:12:16 -0300

    reloj

---
+++ js.js
@@ -0,0 +1,18 @@
+function currentTime() {
+    let date = new Date();
+    let hs = date.getHours();
+    let min = date.getMinutes();
+    let seg = date.getSeconds();
+
+    hs = (hs < 10) ? "0" + hs : hs;
+    min = (min < 10) ? "0" + min : min;
+    seg = (seg < 10) ? "0" + seg : seg;
+
+    let horaActual = `${hs} : ${min} : ${seg}`;
+    let hora = document.getElementById("watch");
+    
+    watch.innerHTML = horaActual;
+}
+
+currentTime();
+setInterval(currentTime, 1000);
